0c023455a79e9e61c2231e95f0bd7909b344d631
Ayoazeez26 <44169294+Ayoazeez26@users.noreply.github.com>
2026-01-22 12:33:49 +0100

    feat: implemented dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import './App.css'
 import Header from './components/Header'
+import { ThemeProvider } from './components/ThemeProvider'
 
 
 function App() {
 
 
   return (
-    <>
-
+    <ThemeProvider>
       <Header />
-
-    </>
+    </ThemeProvider>
   )
 }
 
