ed1ca0dcfbdf13f575b775f117c78176c9f5ad4a
legend4tech <ajuludennis14@gmail.com>
2026-01-22 20:44:57 +0100

    fix: resolved merged conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
+import PriceChart from "./components/PriceChart";
 
 function App() {
   return (
     <>
       <Header />
+      <PriceChart />
     </>
   );
 }
